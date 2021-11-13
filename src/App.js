401cd1c5d4b22292ef9fff6e76ab287602155fd5
unknown <karanrg3005@gmail.com>
2021-11-13 13:11:47 +0530

    6th commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import "./App.css";
-import {useState,useEffect} from "react";
+import { useState, useEffect } from "react";
 import Search from "./components/search";
 import Userrating from "./components/userrating";
 function App() {
   const [userid, setUserid] = useState(null);
   function handleclick(str) {
     setUserid(str);
+    console.log(userid);
   }
   return (
     <>
-       <Search handleclick={handleclick} /> 
-       <Userrating /> 
+      <Search handleclick={handleclick} />
+      <Userrating />
     </>
   );
 }
-
 
 export default App;
--- src/components/search.js
+++ src/components/search.js
@@ -1,28 +1,26 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 
 function Search(props) {
-    const [data,setData]=useState(null)
-    const [print,setPrint]=useState(false)
-    
-      function getData(val)
-      {
-        console.warn(val.target.value)
-        if (setPrint) {
-          setData(val.target.value)
-          props.handleclick(val.target.value)
-        }
-      }
-      return (
-        <div className="App">
-         {
-           print?
-           <h1> {data}</h1>
-           :null
-         }
-        <input type="text"  />
-        <button onClick={()=>setPrint(true)} onClick={getData} >Print Data</button>
-        </div>
-      );
+  const [data, setData] = useState(null);
+  const [print, setPrint] = useState(false);
+
+  function getData(val) {
+    console.warn(val.target.value);
+    if (setPrint) {
+      setData(val.target.value);
+      console.log(val.target.value);
+      props.handleclick(val.target.value);
+    }
+  }
+  return (
+    <div className="App">
+      {print ? <h1> {data}</h1> : null}
+      <input type="text" />
+      <button onClick={() => setPrint(true)} onClick={getData}>
+        Print Data
+      </button>
+    </div>
+  );
 }
 
 export default Search;
